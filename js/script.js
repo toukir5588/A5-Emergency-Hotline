c846e95a07a284f50fe9ed44e7ada19647f93614
Toukir Ahamad <toukirahamad95@gmail.com>
2025-08-28 19:53:34 +0600

    added click function in call btn

--- js/script.js
+++ js/script.js
@@ -1,30 +1,46 @@
 const hearts = document.querySelectorAll(".fa-heart");
 for (const heart of hearts) {
-    heart.addEventListener("click", function () {
-        const availableHeart = parseInt(
-            document.getElementById("live_heart").innerText
-        );
-        const addHeart = availableHeart + 1;
-        const newHeart = (document.getElementById("live_heart").innerText =
-        addHeart);
-        console.log(newHeart);
-    });
+  heart.addEventListener("click", function () {
+    const availableHeart = parseInt(
+      document.getElementById("live_heart").innerText
+    );
+    const addHeart = availableHeart + 1;
+    const newHeart = (document.getElementById("live_heart").innerText =
+      addHeart);
+    console.log(newHeart);
+  });
 }
-// ! end improve heart count section 
+// ! end improve heart count section
 
-
-// ! start remove 20 coin section 
-document.getElementById("call_btn").addEventListener('click',function(){
-    const availableCoin = parseInt(document.getElementById('live_coin').innerText);
+// ? call btn click function
+function getNumberValue(id, serviceName, serviceNumber) {
+  const element = document.getElementById(id).innerText;
+  const convertToNumber = parseInt(element);
+  const availableCoin = convertToNumber;
+  if (availableCoin < 20) {
+    alert("❌ আপনার পর্যাপ্ত কয়েন নেই");
+    return;
+  } else {
     const removeCoin = availableCoin - 20;
-    const serviceName = document.getElementById("service_name").innerText;
-    const serviceNumber = document.getElementById("service_number").innerText;
-    if(availableCoin <= 20){
-        // alert(name)
-        return;
-    }else{
-        alert (serviceName + "    " + serviceNumber)
-        const newCoin = document.getElementById('live_coin').innerText = removeCoin;
-        console.log(newCoin);
-    }
-})
+    const newAvailableCoin = (document.getElementById(id).innerText =
+      removeCoin);
+    alert("📞 calling " + serviceName + "    " + serviceNumber);
+    return newAvailableCoin;
+  }
+}
+
+// ! start remove 20 coin section
+document
+  .getElementById("call_btn_card_1")
+  .addEventListener("click", function () {
+    const serviceName = document.getElementById("service_name_1").innerText;
+    const serviceNumber = document.getElementById("service_number_1").innerText;
+    getNumberValue("live_coin",serviceName,serviceNumber);
+  });
+document
+  .getElementById("call_btn_card_2")
+  .addEventListener("click", function () {
+    const serviceName = document.getElementById("service_name_2").innerText;
+    const serviceNumber = document.getElementById("service_number_2").innerText;
+    getNumberValue("live_coin",serviceName,serviceNumber);
+  });
